bea24863c3b9f5ae447353add32b8680e3d8326f
Masud <masud@datagainservices.com.com>
2021-02-12 17:29:41 +0530

    AuthServer

---
+++ src/AuthServer/AuthServer.Infrastructure/Constants/Roles.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace AuthServer.Infrastructure.Constants
+{
+    public static class Roles
+    {
+        public const string Consumer    = "consumer";
+        public const string Admin       = "admin";
+        public const string Patient     = "patient";
+        public const string Nurse       = "nurse";
+        public const string Dorctor     = "doctor";
+    }
+}
